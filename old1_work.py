66dfbe9b5bd4bbdcdbb3963866af3fdf314ea1dc
sarang <sarangtk1998@gmail.com>
2025-11-20 09:51:10 +0530

    all data

--- old1_work.py
+++ old1_work.py
@@ -6,6 +6,3 @@ print(result)
 
 def add(x,y):
     return x+y
-
-def sub(a,b):
-    return a-b
---
+++ old_work.py
@@ -0,0 +1,22 @@
+commit 9c05d4726a23e3375d6324c502f4baf91894b8d3
+Author: sarang <sarangtk1998@gmail.com>
+Date:   Fri Nov 14 09:44:15 2025 +0530
+
+    updated work.py 9:44
+
+diff --git a/work.py b/work.py
+index 4504b90..19b1650 100644
+--- a/work.py
++++ b/work.py
+@@ -1,4 +1,8 @@
+ data = 34
+ data1 = 78
+ result = data1 + data
+-print(result)
+\ No newline at end of file
++print(result)
++
++
++def add(x,y):
++    return x+y
+\ No newline at end of file
